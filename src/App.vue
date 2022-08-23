803b72a96f9da35f4cbc823746621cfb8edb1140
Rafael <rafaelbianchi06@gmail.com>
2022-08-23 16:10:57 -0300

    v-if, v-else, v-else-if, v-for

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,44 @@
 <!-- eslint-disable prettier/prettier -->
 <script setup>
-import { ref, reactive, onMounted } from 'vue'
-
-const nome = ref('Bill'); // ref = usado normalmente em dados primitivos, ex. string
-
-const person = reactive({ // utilizado normalmente em objetos
-    name: 'Bianchi',
-    age: 37,
-    job: 'Dev',
-})
 
 
-onMounted(()=>{ // reactive
-    person.name = 'Teste de nome'
-    console.log(person)
-})
-
+const show = true
+const array = [1,2,3,4,5,6,7,8,9,10]
 
 </script>
 
 <template>
-  <h2>Composition</h2>
-  <p>{{ nome }}</p>
-  <input
-    v-model="nome"
-    type="text"
-  > <!-- usando ref -->
-  <hr>
-  <p>{{ person.name }}</p> <!-- usando reactive-->
+  <div class="col-6">
+    <h2>Composition</h2>
+    <!--INICIO V-IF, V-ELSE-IF, V-ELSE -->
+    <span v-if="show">V-IF</span>
+    <span
+      v-else-if="show
+        == null"
+    >V-ELSE-IF</span>
+    <span v-else>V-Else</span>
+    <!--FIM V-IF, V-ELSE-IF, V-ELSE -->
+    <br>
+    <!--INICIO V-SHOW -->
+    <span v-show="show">Falando sobre o V-Show</span>
+    <!--INICIO V-SHOW -->
+    <br>
+    <!--INICIO V-FOR -->
+    <ul>
+      <li
+        v-for="item, index in array"
+        :key="index"
+      >
+        {{ item }}
+      </li>
+    </ul>
+    <!--FIM V-FOR -->
+  </div>
 </template>
 
-<style scoped>
+<style>
 </style>
+
+<!-- Diferenças do v-if para o v-show
+O v-if, se a condição não for satisfeita, não renderiza, o que torne mais seguro
+O v-show, com a condição satisfatoria ou não, apresenta no HTML, mas não renderiza o resultado, mais parecido com o CSS display: none-->
