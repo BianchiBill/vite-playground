282a6b1df2ba842f263f100af6a3421a3cc4c754
Rafael <rafaelbianchi06@gmail.com>
2022-08-22 23:18:05 -0300

    feature(vue): ref, reactive onMounted

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,34 @@
+<!-- eslint-disable prettier/prettier -->
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, reactive, onMounted } from 'vue'
+
+const nome = ref('Bill'); // ref = usado normalmente em dados primitivos, ex. string
+
+const person = reactive({ // utilizado normalmente em objetos
+    name: 'Bianchi',
+    age: 37,
+    job: 'Dev',
+})
+
+
+onMounted(()=>{ // reactive
+    person.name = 'Teste de nome'
+    console.log(person)
+})
+
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h2>Composition</h2>
+  <p>{{ nome }}</p>
+  <input
+    v-model="nome"
+    type="text"
+  > <!-- usando ref -->
+  <hr>
+  <p>{{ person.name }}</p> <!-- usando reactive-->
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
